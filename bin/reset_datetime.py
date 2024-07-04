283326e1380d266a0828390380cd513bcb2088ec
Adam Taylor <adam.taylor@sagebase.org>
2024-07-04 17:06:05 +0100

    Refactor reset_datetime

--- bin/reset_datetime.py
+++ bin/reset_datetime.py
@@ -8,15 +8,29 @@ import sys
 import tiffsurgeon
 
 
+# Define a custom argument type for a list of integers
+def list_of_ints(arg):
+    return list(map(int, arg.split(",")))
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Set all DateTime tags in a TIFF to one value (in place).",
+        description="Set all specified date tags in a TIFF to one value (in place).",
     )
     parser.add_argument("image_path", help="TIFF file to process")
     parser.add_argument(
-        "datetime",
+        "--date_tags",
+        help="Comma-separated list of tags (integer ids) with dates to replace (default: 306)",
+        type=list_of_ints,
+        default=[306],
+    )
+    parser.add_argument(
+        "--datetime",
         help="YYYY:MM:DD HH:MM:SS string to replace all DateTime values."
-        "Note the use of colons in the date, as required by the TIFF standard.",
+        " Note the use of colons in the date, as required by the TIFF standard.",
+        type=str,
+        # Defaults to the start of the unix epoch
+        default="1970:01:01 00:00:00",
     )
     argv = sys.argv[1:]
     # Allow date and time to be passed as separate args for convenience.
@@ -48,22 +62,33 @@ def main():
 
     tiff.read_ifds()
 
-    subifds = [
-        tiff.read_ifd(v) for i in tiff.ifds if 330 in i.tags for v in i.tags[330].value
-    ]
-    offsets = [
-        i.tags[306].offset_range.start for i in tiff.ifds + subifds if 306 in i.tags
-    ]
-    if offsets:
-        for x in offsets:
-            tiff.file.seek(x)
-            tiff.file.write(new_datetime)
-        print(
-            f"Successfully replaced {len(offsets)} DateTime values in"
-            f" {args.image_path}"
-        )
-    else:
-        print(f"No DateTime tags found in {args.image_path} -- file not modified")
+    for tag in args.date_tags:
+        if not any(tag in i.tags for i in tiff.ifds):
+            print(f"Tag {tag} not found in {args.image_path} -- file not modified")
+            sys.exit(1)
+
+        subifds = [
+            tiff.read_ifd(v)
+            for i in tiff.ifds
+            if 330 in i.tags
+            for v in i.tags[330].value
+        ]
+        offsets = [
+            i.tags[tag].offset_range.start for i in tiff.ifds + subifds if tag in i.tags
+        ]
+
+        if offsets:
+            for x in offsets:
+                tiff.file.seek(x)
+                tiff.file.write(new_datetime)
+            print(
+                f"Successfully replaced {len(offsets)} values of tag {tag} in"
+                f" {args.image_path}"
+            )
+        else:
+            print(
+                f"No tags with number {tag} found in {args.image_path} -- file not modified"
+            )
 
     tiff.close()
 
